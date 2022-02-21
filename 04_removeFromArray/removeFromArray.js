2f9737119be7b9669106a7999c13e61d461aafe6
edu2907 <eduardo.carneiro2907@gmail.com>
2022-02-21 13:10:49 -0300

    Solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...bannedValues) {
+    for (i = 0; i < bannedValues.length; i++) {
+        for (j = 0; j < arr.length; j++) {
+            if (arr[j] === bannedValues[i]) arr.splice(j,1)
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
