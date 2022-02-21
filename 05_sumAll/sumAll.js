cf32d278c171b5a38adf2466f35393f912c87eef
edu2907 <eduardo.carneiro2907@gmail.com>
2022-02-21 19:17:33 -0300

    Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (...num) {
+    function testValues(element) {
+        return element >= 0 && typeof element === "number"
+    }
+    
+    if (!testValues(num[0]) || !testValues(num[1])) {
+        return "ERROR";
+    }
+    num.sort((num0, num1) => num0 - num1)
+    let sum = 0;
+    for (i = num[0]; i <= num[1]; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
