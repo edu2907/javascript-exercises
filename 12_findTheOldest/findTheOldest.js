41294fe2bc1364bdee00a19b02c7c9986939b417
edu2907 <eduardo.carneiro2907@gmail.com>
2022-02-25 18:05:15 -0300

    Minor edits

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,15 +1,17 @@
 const findTheOldest = function (people) {
     const findAge = person => {
-        if(typeof person.yearOfDeath != "number") person.yearOfDeath = new Date().getFullYear();
+        if(typeof person.yearOfDeath != "number") {
+            person.yearOfDeath = new Date().getFullYear(); 
+        }
         return person.yearOfDeath - person.yearOfBirth
     }
     const compareAges = (previousOlderAge, currentAge) => {
         if (currentAge > previousOlderAge) return currentAge;
         else return previousOlderAge;
     }
-    let peopleAge = people.map(findAge) 
-    let oldestAge = peopleAge.reduce(compareAges, 0)
-    let oldestAgeIndex = peopleAge.indexOf(oldestAge)
+    let peopleAges = people.map(findAge) 
+    let oldestAge = peopleAges.reduce(compareAges, 0)
+    let oldestAgeIndex = peopleAges.indexOf(oldestAge)
     return people[oldestAgeIndex]
 };
 
